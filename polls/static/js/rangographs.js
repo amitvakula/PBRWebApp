af0c53b98f430495a6f8dddfe7e961c967623008
Khang Nguyen <khangnguyen@Khangs-MacBook-Pro.local>
2018-04-13 23:47:34 -0700

    Added mouseover features

--- polls/static/js/rangographs.js
+++ polls/static/js/rangographs.js
@@ -1,6 +1,128 @@
+$(document).ready( function first() {
+  var G3 = new jsnx.Graph();
+
+// patients are green
+G3.addNodesFrom([1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20], {group:0, color: '#33FF33'});
+// symptoms are red
+G3.addNodesFrom(
+[[21,	{name:"dry mouth"}],
+[22,	{name:"dry lips "}],
+[23,	{name:"dry nose"}],
+[24,	{name:"dry skin"}],
+[25,	{name:"brittle nails"}],
+[26,	{name:"pruritus"}],
+[27,	{name:"hair loss"}],
+[28,	{name:"depression"}],
+[29,	{name:"dry eyes"}],
+[30,	{name:"fatigue"}],
+[31,	{name:"hypertriglyceridemia"}],
+[32,	{name:"vertigo"}],
+[33,	{name:"GI upset"}],
+[34,	{name:"facial redness"}],
+[35,	{name:"weight loss"}]]
+    , {group:1, color: '#FF2255'});
+
+
+edges = [];
+edges.push([[1,	23],
+[1,	25],
+[1,	21],
+[1,	25],
+[2,	30],
+[2,	22],
+[2,	21],
+[3,	21],
+[4,	21],
+[4,	27],
+[5,	31],
+[5,	28],
+[5,	32],
+[5,	29],
+[5,	22],
+[5,	25],
+[6,	24],
+[6,	33],
+[7,	22],
+[7,	24],
+[7,	26],
+[8,	34],
+[8,	35],
+[9,	24],
+[9,	22],
+[9,	25],
+[10,	26],
+[11,	22],
+[11,	25],
+[12,	26],
+[13,	27],
+[13,	22],
+[13,	23],
+[13,	25],
+[14,	27],
+[14,	28],
+[14,	23],
+[15,	22],
+[15,	27],
+[16,	22],
+[16,	26],
+[17,	22],
+[17,	27],
+[17,	25],
+[18,	22],
+[18,	29],
+[19,	22],
+[20,	25],
+[20,	22],
+[20,	21]] );
+
+G3.addEdgesFrom(edges[0]);
+
+var color = d3.scale.category20();
+jsnx.draw(G3, {
+    element: '#canvas',
+    layoutAttr: {
+        charge: -150,
+        linkDistance: 50
+    },
+    nodeAttr: {
+        r: 8,
+        title: function(d) { return d.label;},
+        id: function(d) {
+            return 'node-' + d.node;
+        }
+    },
+    nodeStyle: {
+        fill: function(d) {
+            console.log(d.data.color);
+            return d.data.color;
+        },
+        stroke: 'black'
+    },
+    edgeStyle: {
+        stroke: '#999'
+    }
+}, true);
+
+function highlight_nodes(nodes, on) {
+    nodes.forEach(function(n) {
+        d3.select('#node-' + n).style('fill', function(d) {
+            return on ? '#EEE' : d.data.color;
+        });
+    });
+}
+
+d3.selectAll('.node').on('mouseover', function(d) {
+    highlight_nodes(d.G.neighbors(d.node).concat(d.node), true);
+});
+
+d3.selectAll('.node').on('mouseout', function(d) {
+     highlight_nodes(d.G.neighbors(d.node).concat(d.node), false);
+});
+});
+
 $(document).ready( function() {
   // This is a graph generator
-  var G = jsnx.cycleGraph(6);
+  var G = new jsnx.cycleGraph(6);
   // Compute the shortest path between 0 and 4
   var path = jsnx.bidirectionalShortestPath(G, 0, 4);
   // A simple way to color all nodes in the path:
@@ -15,58 +137,19 @@ $(document).ready( function() {
     nodeStyle: {
       fill: function(d) {
         return d.data.color || '#AAA'; // any node without color is gray
-      }
-    }
-  });
- //   // This is a graph generator
- //   var G = jsnx.cycleGraph(6);
- //   // Compute the shortest path between 0 and 4
- //   var path = jsnx.bidirectionalShortestPath(G, 0, 4);
- //   // A simple way to color all nodes in the path:
- //   G.addNodesFrom(path, {color: '#FFF'});
- //   // Color the start and end differently
- //   G.node.get(0).color = '#0F0'; // start is green
- //   G.node.get(4).color = '#F00'; // end is red
- //
- //   jsnx.draw(G, {
- //     element: '#demo-canvas',
- //     withLabels: true,
- //     nodeStyle: {
- //       fill: function(d) {
- //         return d.data.color || '#AAA'; // any node without color is gray
- //       }
- //     }
- // });
-
-  // var G = new jsnx.Graph();
-  //
-  // G.addNodesFrom([1,2,3,4], {group:0});
-  // G.addNodesFrom([5,6,7], {group:1});
-  // G.addNodesFrom([8,9,10,11], {group:2});
-  //
-  // G.addPath([1,2,5,6,7,8,11]);
-  // G.addEdgesFrom([[1,3],[1,4],[3,4],[2,3],[2,4],[8,9],[8,10],[9,10],[11,10],[11,9]]);
-  //
-  // var color = d3.scale.category20();
-  // jsnx.draw(G, {
-  //     element: '#canvas',
-  //     layoutAttr: {
-  //         charge: -120,
-  //         linkDistance: 20
-  //     },
-  //     nodeAttr: {
-  //         r: 5,
-  //         title: function(d) { return d.label;}
-  //     },
-  //     nodeStyle: {
-  //         fill: function(d) {
-  //             return color(d.data.group);
-  //         },
-  //         stroke: 'none'
-  //     },
-  //     edgeStyle: {
-  //         fill: '#999'
-  //     }
-  // }, true);
-
+        }
+      },
+    // weighted: true,
+    // nodeShape: "image",
+    // nodeAttr: {
+    //     "xlink:href": "https://github.com/favicon.ico",
+    //     width: 20,
+    //     height: 20,
+    //     x: -8,
+    //     y: -8
+    //     },
+    // edgeStyle: {
+    //     'stroke-width': 10
+    //     }
+    });
 });
